47506514de5b2faf7ee025a86b193f0b997f615a
Dilli Bhandari <bhandaridilli1@gmail.com>
2021-03-22 23:24:15 +0545

    manage table with js

--- app.js
+++ app.js
@@ -1,3 +1,37 @@
+// var API_URL='https://api.github.com/users/rairupak98/repos'
+// function getData()
+// {
+//     fetch(,{
+//         method='GET'
+//     })
+// }
+// window.onload=function () {
+//     getdata();
+// };
+// let API='https://api.github.com/users/dillibhai/repos';
+// function getdata()
+// {
+// fetch(API)
+// .then((res)=>res.json())
+// .then((res)=>displayData(res))
+// }
+// function displayData(data){
+//     let tbody=document.getElementById("dataBody");
+//     console.log(data);
+//     data.map((item,index)=>{
+// var row=tbody.insertRow(index);
+// var name=row.insertCell(0);
+// var fullname=row.insertCell(1);
+// var url=row.insertCell(2);
+// var language=row.insertCell(3);
+// name.innerHTML=item.name;
+// fullname.innerHTML=item.full_name;
+// name.innerHTML=item.name;
+// url.innerHTML=item.html_url;
+// language.innerHTML=`<a href="$val.html_url}" target="_blank">Repo name</a>`;
+//     });
+// }
+
 function fetchData(){
     fetch("https://api.github.com/users/dillibhai/repos")
     .then(response =>{
@@ -12,23 +46,19 @@ function fetchData(){
         const html = data.map(user =>{
             return `
            
-
-
-<table class="styled-table">
+<table>
     <thead>
-        <tr>
-            <th>ID:</th>
-            <th>Name</th>
-            <th>User type</th>
-            <th>Login</th>
-        </tr>
+        
+        
+    
     </thead>
     <tbody>
         <tr>
             <td>${user.id}</td>
             <td>${user.name}</td>
             <td>${user.type}</td>
-            <td>${user.login}</td>
+            <td>${user.login}</td>         
+           <td> <a href="${user.html_url}">${user.html_url}</a></td>
         </tr>
     </tbody>
 </table>
